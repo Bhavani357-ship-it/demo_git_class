d5c0fc43fc5211e4349a4603a21159850da8b52d
bhavani <bhavanibhavani27316@gmail.com>
2025-05-04 14:47:09 +0530

    storing empty file

---
+++ bitbucket-repos/weekend-devops-8am-feb/docker-install.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+echo "installing docker service in amazon linux 2023 server"
+sudo dnf update -y
+sudo yum update -y
+sudo yum install -y docker
+sudo yum install -y git
+echo "package installed and restarting the service"
+sudo systemctl restart docker
+sudo systemctl enable docker
+sudo systemctl status docker
+sudo hostname Docker-Server-Weekend
+sudo echo "Docker-Server-Weekend" > /etc/hostname
+sudo bash
